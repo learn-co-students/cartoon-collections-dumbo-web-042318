692126f916ab2da6e2fbe8236894702c6a184a88
John Chriest <johnchriest@gmail.com>
2018-04-13 22:34:34 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index {|value, index|
+    puts "#{index + 1} #{value}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  array = planeteer_calls.map! {|w| w.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long.any? do |x|
+    x.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array = cheese & cheese_types
+  array[0]
 end
+
+=begin
+This is another way I found to do what the assignment asked
+
+def find_the_cheese(cheese)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array = cheese & cheese_types
+  array[0]
+end
+=end
